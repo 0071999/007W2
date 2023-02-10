bd8e8a3f7bfc3ebabb48cb8db8eca8c5355831a5
James <0071999@schoolsnet.act.edu.au>
2023-02-10 14:16:33 +1100

    updated hello.php

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php
+
+function footer():string
+{
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
+}
+
+
+?>
