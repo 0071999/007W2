276d540fcb579dda8925b4b91ffa6ef7a931416f
James <0071999@schoolsnet.act.edu.au>
2023-02-17 15:30:27 +1100

    init contact

--- contact.php
+++ contact.php
@@ -1,46 +1,50 @@
 <!doctype html>
 <?php include "template.php" ?>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact Us</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <h1>Contact Us</h1>
 <div class="container-fluid">
-    <h1 class="text-primary">Please Send us a Message</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <form action="contact.php" method="post">
         <div class="mb-3">
-            <label for="contactEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="contactEmail" name="contactEmail"
-                   placeholder="name@example.com">
+            <label for="inputEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="contactMessage" class="form-label">Message</label>
-            <textarea class="form-control" id="contactMessage" name="contactMessage" rows="3"></textarea>
+            <label for="inputMessage" class="form-label">Message</label>
+            <input type="text" class="form-control" id="inputMessage" name="inputMessage">
         </div>
-        <button type="submit" name="formSubmit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 
-
 <?php
-if (isset($_POST['formSubmit'])) {
-    $userEmail = sanitiseData($_POST['contactEmail']);
-    $userMessage = sanitiseData($_POST['contactMessage']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userEmail = sanitiseData($_POST['InputEmail']);
+    $userMessage = sanitiseData($_POST['InputMessage']);
 
-   echo $userEmail;
-   echo"<p>";
-   echo $userMessage;
+
+
+    $formError = false;
+    if (empty($_POST['inputEmail'])) {
+        $formError = true;
+        echo "Enter an email address.";
+    }
+    if (empty($_POST['inputMessage'])) {
+        $formError = true;
+        echo "Enter a message to submit.";
+    }
+    if ($formError == false) {
+        $csvFile = fopen("contact.csv","a");
+        fwrite($csvFile, $userEmail.",".$userMessage."\n");
+        fclose($csvFile);
+    }
 }
 
 ?>
 
-
-
-
+</p>
 
 <?php echo footer() ?>
 </body>
