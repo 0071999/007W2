8b780cb78ea7ce680bac727fd35d66aa2471a982
James <0071999@schoolsnet.act.edu.au>
2023-03-29 11:58:44 +1100

    Register code

--- template.php
+++ template.php
@@ -20,6 +20,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact Us</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
 
             </ul>
 
